4e5f882489d42bb02b3a36a369c75fa87c3a0668
salsa <salsaprmudta@gmail.com>
2022-11-14 15:49:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css"
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Skills from "./routes/Skills";
+import Blog from "./routes/Blog";
+
+import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/home" element={<Home />} /> 
+      <Route path="/about" element={<About />} /> 
+      <Route path="/skills" element={<Skills />} /> 
+      <Route path="/blog" element={<Blog />} /> 
+      <Route />  
+    
+    </Routes>  
+    </>
+
   );
 }
 
